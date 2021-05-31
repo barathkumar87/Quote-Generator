30b914f871ca5b2dab5c336153afb48ff242317a
Barath Kumar P S <73652035+barathkumar87@users.noreply.github.com>
2021-05-31 09:02:42 +0530

    index

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-
-const text = document.querySelector('.quote');
-const auth = document.querySelector('.author');
-const button = document.querySelector('.twitter-share-button');
-const nextBtn = document.querySelector('.next');
-// const loader = document.querySelector('.loader');
 $(document).ready(function() {
 	
 	$("#btn").on("click", function(){
@@ -40,21 +34,3 @@ $(document).ready(function() {
 	});
 
 });
-const getQuote = async () => {
-    
-    // loader.classList.remove('hide');
-    const res = await fetch(`https://type.fit/api/quotes`);
-    const quotes = await res.json();
-    // loader.classList.add('hide');
-    const num = Math.floor(Math.random()*quotes.length);
-    const item = quotes[num];
-    const quote = item.text;
-    const author = item.author;
-    text.innerText = quote;
-    auth.innerText = author;
-    button.href = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
-}
-
-nextBtn.addEventListener('click', getQuote);
-
-getQuote();
